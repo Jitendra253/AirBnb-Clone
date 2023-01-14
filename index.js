566566c16e00cf6d79e4ea2802a1c4fa54948ec9
Jitendra253 <107558211+Jitendra253@users.noreply.github.com>
2023-01-15 02:39:25 +0530

    final change

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const flash = require('express-flash');
 const mainroute = require('./routes/productRoute')
 const loginRoute = require('./routes/loginRoute');
+const adminRoute = require('./routes/adminRoute')
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -16,13 +17,13 @@ app.use(session({
 }));
 app.use(flash());
 
-
 app.set('view engine', 'ejs')
 app.use('/public', express.static('public'));
 
 
 app.use(mainroute);
 app.use(loginRoute);
+app.use(adminRoute);
 
 app.get('/help', (req, res) => {
     res.render('help')
